b5a9a9f4af0a752dd55069f0a3ecf3bd71cd2470
Pengalu <pengalu99@gmail.com>
2022-10-07 00:09:42 -0700

    z indexes WHY

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 
 var btn = document.getElementById("1");
 var btn2 = document.getElementById("2");
-//var btn3 = document.getElementById("3");
+var btn3 = document.getElementById("3");
 console.log(btn);
 console.log(btn2);
-//console.log(btn3);
-/*
-btn.addEventListener('click', function btn1Clicked(event) {
+console.log(btn3);
 
+function btn1Clicked() {
     btn.style.animation = null;
   
     
@@ -33,13 +32,39 @@ btn.addEventListener('click', function btn1Clicked(event) {
    
 
 
-}, false);
-*/
+}
+function btn3Clicked() {
+
+    btn3.style.animation = null;
+
+
+    console.log("hi 1");
+    btn3.style.animationDuration = "2s";
+    btn3.style.animation = 'main_bar_collapse';
+    btn3.style.animationDuration = "2s";
+    btn3.style.animationFillMode = "forwards";
+
+    btn2.style.animationDuration = "2s";
+    btn2.style.animation = 'main_bar_collapse'; //this is a nasty ah language bruh :skull:
+    btn2.style.animationFillMode = "forwards";
+    btn2.style.animationDuration = "2s";
+    btn2.style.animationDelay = "0.2s";
+    btn2.style.top = "20%";
+
+    btn.style.animationDuration = "2s";
+    btn.style.animation = 'main_bar_collapse'; //this too. :skull:
+    btn.style.animationFillMode = "forwards";
+    btn.style.animationDuration = "2s";
+    btn.style.animationDelay = "0.3s";
+    btn.style.top = "20%";
+
+}
+
 
 function btn2Clicked() {
     btn2.addEventListener('click', btn2Clicked, false);
     console.log("hi");
-   /* btn2.style.animation = null;
+   btn2.style.animation = null;
 
 
 
@@ -61,12 +86,11 @@ function btn2Clicked() {
     btn3.style.animationDuration = "2s";
     btn3.style.animationDelay = "0.3s";
     btn3.style.top = "20%";
-*/
+
 
 
 }
-btn.addEventListener('click', btn2Clicked, false);
-btn2.addEventListener('click', btn2Clicked, false);
+
 //btn2.addEventListener("click",btn2Clicked,false);
 //btn2.addEventListener('click', function btn2Clicked(event) {
     //console.log ("hi");
